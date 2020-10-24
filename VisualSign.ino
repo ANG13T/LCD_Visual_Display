ca9958eb66b8c9c176759d0490bd7f9198ae5580
angelina-tsuboi <angelina.t1832@gmail.com>
2020-10-24 08:34:01 -0700

    lcd working

--- VisualSign.ino
+++ VisualSign.ino
@@ -1,15 +1,24 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
 const int SW_pin = 2; // digital pin connected to switch output
 const int X_pin = A0; // analog pin connected to X output
 const int Y_pin = A1; // analog pin connected to Y output
+
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(SW_pin, INPUT); //joystick inout
   digitalWrite(SW_pin, HIGH);
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
+  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
+  lcd.print("Hello World!");
   // put your main code here, to run repeatedly:
   Serial.print("Switch:  ");
   Serial.print(digitalRead(SW_pin));
