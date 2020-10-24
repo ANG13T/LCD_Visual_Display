cc4d89aea03a5e26fd593bac3f37ba9dc15ff487
angelina-tsuboi <angelina.t1832@gmail.com>
2020-10-24 08:42:28 -0700

    added push button functionality

--- VisualSign.ino
+++ VisualSign.ino
@@ -4,12 +4,14 @@
 const int SW_pin = 2; // digital pin connected to switch output
 const int X_pin = A0; // analog pin connected to X output
 const int Y_pin = A1; // analog pin connected to Y output
+const int buttonPin = 3; //button input pin
 
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(SW_pin, INPUT); //joystick inout
+  pinMode(buttonPin, INPUT);//button input
   digitalWrite(SW_pin, HIGH);
   Serial.begin(9600);
   lcd.init();
@@ -17,17 +19,19 @@ void setup() {
 }
 
 void loop() {
+  
   lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
   lcd.print("Hello World!");
   // put your main code here, to run repeatedly:
-  Serial.print("Switch:  ");
-  Serial.print(digitalRead(SW_pin));
-  Serial.print("\n");
-  Serial.print("X-axis: ");
-  Serial.print(analogRead(X_pin));
-  Serial.print("\n");
-  Serial.print("Y-axis: ");
-  Serial.println(analogRead(Y_pin));
-  Serial.print("\n\n");
-  delay(500);
+//  Serial.print("Switch:  ");
+//  Serial.print(digitalRead(SW_pin));
+//  Serial.print("\n");
+//  Serial.print("X-axis: ");
+//  Serial.print(analogRead(X_pin));
+//  Serial.print("\n");
+//  Serial.print("Y-axis: ");
+//  Serial.println(analogRead(Y_pin));
+//  Serial.print("\n\n");
+//  delay(500);
+    Serial.println(digitalRead(buttonPin));
 }
